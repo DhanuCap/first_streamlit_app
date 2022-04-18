e6419846afd5004738f0e75115e73db3d960c2ed
Dhanunjaya <103937458+DhanuCap@users.noreply.github.com>
2022-04-18 18:19:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Eggs')
+streamlit.text('Spinach')
+streamlit.text('Watermelon')
